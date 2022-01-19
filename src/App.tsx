39cf481ced588bd6daba4d72ea2a4567d5c4a9ab
Viktor Nagy <viktor.nagy@01people.com>
2022-01-19 13:38:00 +0100

    feat: testing constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { TIndexes, TItemDefinition } from './App.d';
+
+const items: TItemDefinition = [
+  {
+    name: "Reddit",
+    color: "red",
+    icon: "Reddit",
+    url: "https://www.reddit.com",
+  },
+  {
+    name: "Facebook",
+    color: "blue",
+    icon: "Facebook",
+    url: "https://www.facebook.com",
+  },
+];
+
+const indexes: TIndexes = [
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+  [0,0,0,0,0,1,1,1,1,1],
+];
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
