3da3760c8566051baba1e815d4859357ad67ed60
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-01-20 12:38:02 +0100

    feat: gaps + border radius + more complex example

--- src/components/piece/Piece.tsx
+++ src/components/piece/Piece.tsx
@@ -2,6 +2,8 @@ import { CSSProperties } from "react";
 import "./Piece.css";
 import { TPieceState } from './Piece.d';
 
+const GAP = 0.2;
+
 export const Piece = ({ x, y, x2, y2, item }: TPieceState) => {
   const top = y * 10;
   const left = x * 10;
@@ -9,11 +11,11 @@ export const Piece = ({ x, y, x2, y2, item }: TPieceState) => {
   const height = (y2 - y + 1) * 10;
 
   const styles: CSSProperties = {
-    top: `${top}vh`,
-    left: `${left}vw`,
-    width: `${width}vw`,
-    height: `${height}vh`,
-    lineHeight: `${height}vh`,
+    top: `${top+GAP}vh`,
+    left: `${left+GAP}vw`,
+    width: `${width-GAP*2}vw`,
+    height: `${height-GAP*2}vh`,
+    lineHeight: `${height-GAP*2}vh`,
     backgroundColor: item.color,
   };
 
